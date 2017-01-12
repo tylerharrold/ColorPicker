f8359dc7ec288b53dff41d0aeb60a77045ca4c22
tylerharrold <tylerharrold@gmail.com>
2017-01-12 18:05:00 -0500

    basic functionality of color squares added

---
+++ javascript/colorBox.js
@@ -0,0 +1,26 @@
+class ColorBox{
+
+  static setColor(domObject){
+    domObject.style.background = ColorBox.getRandomColor();
+  }
+
+  static setDisplay(domObject , value){
+    domObject.style.display = value;
+  }
+
+  static setAsWinner(domObject){
+    domObject.isWinner = true;
+  }
+
+  static getRandomColor(){
+    var r = Math.ceil((Math.random() * 255));
+    var g = Math.ceil((Math.random() * 255));
+    var b = Math.ceil((Math.random() * 255));
+    return "rgb(" + r + "," + g + "," + b + ")";
+  }
+
+  static fade(domObject){
+    domObject.style.background = "#111a28";
+  }
+
+}
---
+++ javascript/game.js
@@ -0,0 +1,56 @@
+class Game{
+  constructor(){
+    this.colorBoxes = [];
+    this.numberOfColors = 6;
+  }
+  init(){
+    this.setupColorBoxes();
+    this.populateNewColors();
+    this.setupWinningBox();
+  }
+
+  // creates and stores all color box objects for game
+  setupColorBoxes(){
+    // grab hold of .square DOM objects
+    this.colorBoxes = document.querySelectorAll(".square");
+
+    // give each box a listener
+    for(var i = 0 ; i < this.colorBoxes.length ; i++){
+      this.colorBoxes[i].addEventListener("click" , this.colorBoxEvent);
+    }
+  }
+
+  // populate new colors, disable uncolored boxes
+  populateNewColors(){
+    // give a new color to every relevant box
+    for(var i = 0 ; i < this.colorBoxes.length ; i++){
+      if(i < this.numberOfColors){
+        ColorBox.setColor(this.colorBoxes[i]);
+      }
+      // set the display of the color box appropriately
+      var displayVal;
+      (i < this.numberOfColors) ? displayVal = "block" : displayVal = "none";
+      ColorBox.setDisplay(this.colorBoxes[i] , displayVal);
+    }
+  }
+
+  // randomly sets a winning box
+  setupWinningBox(){
+    // get random number randing from 0 to numberOfColors
+    var winningBox = Math.floor(Math.random() * this.numberOfColors);
+    ColorBox.setAsWinner(this.colorBoxes[winningBox]);
+  }
+
+  // event handler for the colored boxes
+  colorBoxEvent(){
+    if(this.isWinner == true){
+      alert("winner");
+    }
+    else{
+      ColorBox.fade(this);
+    }
+  }
+
+  // method to fade out an incorrectly clicked box
+
+}
